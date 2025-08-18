75d51a8ac876e1379b91f174e0b6346efb4069d9
yljos <yljos@outlook.com>
2025-08-18 18:11:39 +0800

    ddns-go

--- stow.sh
+++ stow.sh
@@ -4,6 +4,7 @@ ln -sf /root/openwrt/openwrt/root/sing-box.sh /root/sing-box.sh
 ln -sf /root/openwrt/openwrt/root/tproxy_1.0.sh /root/tproxy_1.0.sh
 ln -sf /root/openwrt/openwrt/root/update_openwrt.sh /root/update_openwrt.sh
 ln -sf /root/openwrt/openwrt/root/json.sh /root/json.sh
+ln -sf /root/openwrt/openwrt/root/ddns-go /root/ddns-go
 # 配置文件链接到 /etc
 rm -rf /etc/config/dhcp
 ln -sf /root/openwrt/openwrt/etc/config/dhcp /etc/config/dhcp
