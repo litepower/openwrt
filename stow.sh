c29e19617ec4a3c53efe519d4c83b5c67929748f
yljos <yljos@outlook.com>
2025-08-18 16:22:04 +0800

    add stow.sh

---
+++ stow.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# 脚本链接到 /root
+ln -sf /root/openwrt/openwrt/root/sing-box.sh /root/sing-box.sh
+ln -sf /root/openwrt/openwrt/root/tproxy_1.0.sh /root/tproxy_1.0.sh
+ln -sf /root/openwrt/openwrt/root/update_openwrt.sh /root/update_openwrt.sh
+
+# 配置文件链接到 /etc
+rm -rf /etc/config/dhcp
+ln -sf /root/openwrt/openwrt/etc/config/dhcp /etc/config/dhcp
+
+# nftables 配置目录
+
+ln -sf /root/openwrt/openwrt/etc/nftables.d/11-sing-box.nft /etc/nftables.d/11-sing-box.nft
