6f069749de06d8907f04bf687bb75d7facfbffdb
yljos <yljos@outlook.com>
2025-08-19 08:20:37 +0800

    add hompage

--- stow.sh
+++ stow.sh
@@ -8,6 +8,10 @@ ln -sf /root/openwrt/openwrt/root/ddns-go /root/ddns-go
 # 配置文件链接到 /etc
 rm -rf /etc/config/dhcp
 ln -sf /root/openwrt/openwrt/etc/config/dhcp /etc/config/dhcp
+rm -rf /etc/config/rpcd
+ln -sf /root/openwrt/openwrt/etc/config/rpcd /etc/config/rpcd
+rm -rf /usr/share/rpcd/acl.d/homepage.json
+ls -sf /root/openwrt/openwrt//usr/share/rpcd/acl.d/homepage.json /usr/share/rpcd/acl.d/homepage.json
 
 # nftables 配置目录
 rm -rf /etc/nftables.d/11-sing-box.nft
