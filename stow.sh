9ab592f1d330be6b25688b5c8aebe892959e5153
yljos <yljos@outlook.com>
2025-08-22 12:19:23 +0800

    improve tproxy

--- stow.sh
+++ stow.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # 脚本链接到 /root
 ln -sf /root/openwrt/openwrt/root/sing-box.sh /root/sing-box.sh
-ln -sf /root/openwrt/openwrt/root/tproxy_1.0.sh /root/tproxy_1.0.sh
 ln -sf /root/openwrt/openwrt/root/update_openwrt.sh /root/update_openwrt.sh
 ln -sf /root/openwrt/openwrt/root/json.sh /root/json.sh
 ln -sf /root/openwrt/openwrt/root/ddns-go /root/ddns-go
@@ -12,6 +11,7 @@ rm -rf /etc/config/rpcd
 ln -sf /root/openwrt/openwrt/etc/config/rpcd /etc/config/rpcd
 rm -rf /usr/share/rpcd/acl.d/homepage.json
 ln -sf /root/openwrt/openwrt//usr/share/rpcd/acl.d/homepage.json /usr/share/rpcd/acl.d/homepage.json
+ln -sf /root/openwrt/openwrt/etc/init.d/tproxy /etc/init.d/tproxy
 
 # nftables 配置目录
 rm -rf /etc/nftables.d/11-sing-box.nft
